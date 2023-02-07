09732d27f7fd685f67f0c4cbf2fe94ea690cdc40
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-02-07 12:02:53 -0300

    feat: :clown_face: Mocks de Chamadas à API para evitar chamadas excessívas durante o desenvolvimeto

---
+++ src/mocks/user.ts
@@ -0,0 +1,28 @@
+export const user = {
+  "country": "BR",
+  "display_name": "Alexsandro Pinheiro",
+  "email": "alexsandrofire@hotmail.com",
+  "explicit_content": {
+      "filter_enabled": false,
+      "filter_locked": false
+  },
+  "external_urls": {
+      "spotify": "https://open.spotify.com/user/2137mz3pshv45xc7nxkytznoq"
+  },
+  "followers": {
+      "href": null,
+      "total": 0
+  },
+  "href": "https://api.spotify.com/v1/users/2137mz3pshv45xc7nxkytznoq",
+  "id": "2137mz3pshv45xc7nxkytznoq",
+  "images": [
+      {
+          "height": null,
+          "url": "https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=1108384172627912&height=300&width=300&ext=1678096809&hash=AeSZgCMfPkeoNLIuGQY",
+          "width": null
+      }
+  ],
+  "product": "premium",
+  "type": "user",
+  "uri": "spotify:user:2137mz3pshv45xc7nxkytznoq"
+}
