418fad69e01ffb71595f02982c3c6bcde31193cb
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-02-10 20:12:50 -0300

    feat: :sparkles: Componente de card para renderizar as playlists

---
+++ src/components/PlaylistCard.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image"
+
+import styles from '@/styles/components/PlaylistCard.module.css'
+import { IPlaylist } from "@/interfaces/types"
+
+function PlaylistCard({ playlist }: { playlist: IPlaylist }) {
+  return (
+    <div className={styles.container}>
+      <div className={styles['img-container']} >
+        <Image
+          src={playlist.images[0].url}
+          alt={playlist.name}
+          width='300'
+          height='300'
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.info}>
+        <p>
+          <strong>
+            {
+              playlist.name
+            }
+          </strong>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default PlaylistCard
