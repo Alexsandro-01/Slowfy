bf6295993f1a645b3fd5d3fd7113d421c4923e9b
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-02-21 20:33:34 -0300

    refactor: :recycle: Quebra a função topItems em duas funções separadas
    uma par musics e outra para artists

--- src/utils/fetchs/spotify.ts
+++ src/utils/fetchs/spotify.ts
@@ -1,54 +1,65 @@
-import { IArtists, IMusics, IPlaylists, IRecentMusics, IRefreshedToken, ITopItems, ITopItemsPeriod, IUserProfile } from '@/interfaces/types';
+import { IArtists, ILimit, IMusics, IPlaylists, IRecentMusics, IRefreshedToken, IPeriod, IUserProfile } from '@/interfaces/types';
 
 
 export async function fetchProfile(code: string): Promise<IUserProfile> {
 
-	const result = await fetch('https://api.spotify.com/v1/me', {
-		method: 'GET', headers: { Authorization: `Bearer ${code}` }
-	});
+  const result = await fetch('https://api.spotify.com/v1/me', {
+    method: 'GET', headers: { Authorization: `Bearer ${code}` }
+  });
 
-	return await result.json();
+  return await result.json();
 }
 
 export async function fetchUserPlaylists(code: string): Promise<IPlaylists> {
 
-	const result = await fetch('https://api.spotify.com/v1/me/playlists', {
-		method: 'GET', headers: { Authorization: `Bearer ${code}` }
-	});
+  const result = await fetch('https://api.spotify.com/v1/me/playlists', {
+    method: 'GET', headers: { Authorization: `Bearer ${code}` }
+  });
 
-	return await result.json();
+  return await result.json();
 }
 
-export async function fetchTopItems(code: string, item: ITopItems, period: ITopItemsPeriod): Promise<IArtists | IMusics> {
-	const result = await fetch(`https://api.spotify.com/v1/me/top/${item}?limit=10&time_range=${period}`, {
-		headers: {
-			Authorization: `Bearer ${code}`,
-			'Content-Type': 'application/json'
-		}
-	});
-    
-	return await result.json();
+export async function fetchTopMusics(code: string, period: IPeriod, limit: ILimit): Promise<IArtists | IMusics> {
+  const result = await fetch(`https://api.spotify.com/v1/me/top/trascks?limit=${limit}&time_range=${period}`, {
+    headers: {
+      Authorization: `Bearer ${code}`,
+      'Content-Type': 'application/json'
+    }
+  });
+
+  return await result.json();
+}
+
+export async function fetchTopArtists(code: string, period: IPeriod, limit: number): Promise<IArtists | IMusics> {
+  const result = await fetch(`https://api.spotify.com/v1/me/top/artists?limit=${limit}&time_range=${period}`, {
+    headers: {
+      Authorization: `Bearer ${code}`,
+      'Content-Type': 'application/json'
+    }
+  });
+
+  return await result.json();
 }
 
 export async function fetchRecentMusicPlayed(code: string, limit: number): Promise<IRecentMusics> {
-	const result = await fetch(`https://api.spotify.com/v1/me/player/recently-played?limit=${limit}`, {
-		headers: {
-			Authorization: `Bearer ${code}`,
-			'Content-Type': 'application/json'
-		}
-	});
+  const result = await fetch(`https://api.spotify.com/v1/me/player/recently-played?limit=${limit}`, {
+    headers: {
+      Authorization: `Bearer ${code}`,
+      'Content-Type': 'application/json'
+    }
+  });
 
-	return await result.json();
+  return await result.json();
 }
 
 export async function fetchRefreshToken(refreshToken: string, refreshTokenUrl: string): Promise<IRefreshedToken> {
-	const response = await fetch(refreshTokenUrl as string, {
-		method: 'POST',
-		body: JSON.stringify({
-			'refresh_token': refreshToken,
-		})
-	});
+  const response = await fetch(refreshTokenUrl as string, {
+    method: 'POST',
+    body: JSON.stringify({
+      'refresh_token': refreshToken,
+    })
+  });
 
-	const data = await response.json();
-	return data;
+  const data = await response.json();
+  return data;
 }
